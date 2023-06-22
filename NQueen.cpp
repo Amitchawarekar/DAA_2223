f966743397845be8de84708b4cacc7dbc2ca71d8
Amit Chawarekar <54398300+Amitchawarekar@users.noreply.github.com>
2023-06-22 18:55:43 +0530

    Update NQueen.cpp

--- NQueen.cpp
+++ NQueen.cpp
@@ -1,77 +1,76 @@
-//N Queen Problem
+#include <bits/stdc++.h>
 
-#include<iostream>
 using namespace std;
 
-bool isSafe(int** arr,int x,int y,int n){
-	// For columns
-	for(int row= 0 ;row<x;row++){
-		if(arr[row][y]==1){
-			return false;
-		}
-	}
-	
-	//Upper Left diagonal
-	int row = x;
-	int col = y;
-	while(row>=0 && col >=0){
-		if(arr[row][col]==1){
-			return false;
-		}
-		row--;
-		col--;
-	}
-	
-	//Upper Right Diagonal
-	row = x ;
-	col = y;
-	while(row>=0 && col < n){
-		if(arr[row][col]==1){
-			return false;
-		}
-		row--;
-		col++;
-	}
-	return true;
-}
-// x is for keeping count of rows
-// y is for keeping count of columns
+class NQueens{
+    int n;
+    vector<vector<string> > res;
 
-bool nQueen(int** arr,int x,int n){
-	if(x >= n){
-		return true;
-	}
-	for(int col=0;col<n;col++){
-		if(isSafe(arr,x,col,n)){
-			arr[x][col]=1;
-			
-			if(nQueen(arr,x+1,n)){
-				return true;
-			}
-			arr[x][col]=0; //backtracking
-		}
-	}
-	return false;
-}
+    public:
+        NQueens(int a){
+            n = a;
+            vector<string> curr(n);
+            for(int i=0; i<n; i++) curr[i] = string(n, '.');
+            solve(curr, 0);
+            print();
+        }
+
+        void solve(vector<string> curr, int col){
+            if(col==n){
+                res.push_back(curr);
+                return;
+            }
+
+            for(int i=0; i<n; i++){
+                if(check(curr, i, col)){
+                    curr[i][col] = 'Q';
+                    solve(curr, col+1);
+                    curr[i][col] = '.';
+                }
+            }
+        }
+
+        bool check(vector<string>& curr, int row, int col){
+            int c=col-1, r;
+            while(c>=0){
+                if(curr[row][c]=='Q') return false;
+                c--;
+            }
+            
+            c=col-1;
+            r=row-1;
+            while(c>=0 && r>=0){
+                if(curr[r][c]=='Q') return false;
+                r--;
+                c--;
+            }
+
+            c=col-1;
+            r=row+1;
+            while(c>=0 && r<n){
+                if(curr[r][c]=='Q') return false;
+                r++;
+                c--;
+            }
+            
+            return true;
+        }
+
+        void print(){
+            for(auto temp: res){
+                for(string str: temp){
+                    cout<<str<<endl;
+                }
+                cout<<endl<<string(n+4, '-')<<endl;
+            }
+        }
+};
+
 int main(){
-	cout << "Enter the size of the chess board: ";
-	int n;
-	cin>>n;
-	int** arr= new int* [n];
-	for(int i=0;i<n;i++){
-		arr[i] = new int [n];
-		for(int j=0;j<n;j++){
-			arr[i][j]=0;
-		}
-	}
-	cout << "\nOutput: \n"<<endl;
-	if(nQueen(arr,0,n)){
-		for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cout<<arr[i][j]<<" ";
-		}cout << endl;
-	}
-	}
-	
-	return 0;
+    int a;
+    cout<<"\nEnter the dimension of the board: ";
+    cin>>a;
+    NQueens n(a);
+
+    return 0;
 }
